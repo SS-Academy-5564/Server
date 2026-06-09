2d43bbae47e3d7779f3e88b671438feb62cdb39c
Artem Koval <kovalartem9010@gmail.com>
2026-06-09 17:36:26 +0300

    feat: configure email service with options and validation

--- Pulse.BL/DependencyInjection.cs
+++ Pulse.BL/DependencyInjection.cs
@@ -1,5 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+<<<<<<< HEAD
+=======
+using Microsoft.Extensions.Options;
+>>>>>>> 33f6239 (feat: configure email service with options and validation)
 using Pulse.BL.Feature.Email;
 
 namespace Pulse.BL;
