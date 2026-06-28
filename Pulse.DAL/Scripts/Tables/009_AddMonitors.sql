d1c65751da8bd98eaa3029280364267222e125ba
pichus <dimatyzuk@gmail.com>
2026-06-28 19:32:57 +0300

    feat: add migration which introduces a table for monitors

---
+++ Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
+    BEGIN
+        CREATE TABLE dbo.Monitors
+        (
+            Id              UNIQUEIDENTIFIER NOT NULL DEFAULT NEWSEQUENTIALID(),
+            Name            VARCHAR(64)      NOT NULL,
+            Url             NVARCHAR(2083)   NOT NULL,
+            CurrentValue    NVARCHAR(MAX),
+            LastCheckedAt   DATETIMEOFFSET,
+            StatusId        UNIQUEIDENTIFIER NOT NULL,
+            PollingInterval INT              NOT NULL,
+
+            CONSTRAINT PK_Monitors_Id PRIMARY KEY (Id),
+            CONSTRAINT FK_Monitors_Status_Id FOREIGN KEY (StatusId) REFERENCES dbo.MonitorStatuses (Id),
+            CONSTRAINT CK_Monitors_Interval CHECK (PollingInterval > 0)
+        );
+    END;
