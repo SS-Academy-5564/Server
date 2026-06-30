7af48ee701b19bfdc35bdc936e64eb51efb46d84
pichus <dimatyzuk@gmail.com>
2026-06-30 16:18:28 +0300

    fix: a typo in CK_Monitors_PollingTimeoutSeconds

--- Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
+++ Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
@@ -16,7 +16,7 @@ IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
             CONSTRAINT PK_Monitors_Id PRIMARY KEY (Id),
             CONSTRAINT FK_Monitors_Status_Id FOREIGN KEY (StatusId) REFERENCES dbo.MonitorStatuses (Id),
             CONSTRAINT CK_Monitors_PollingIntervalSeconds CHECK (PollingIntervalSeconds BETWEEN 60 AND 24 * 60 * 60),
-            CONSTRAINT CK_Monitors_PollingTimeoutSeconds CHECK (PollingIntervalSeconds BETWEEN 5 AND 30),
+            CONSTRAINT CK_Monitors_PollingTimeoutSeconds CHECK (PollingTimeoutSeconds BETWEEN 5 AND 30),
             CONSTRAINT CK_Monitors_ResultPath CHECK (ResultPath BETWEEN 1 AND 255),
             CONSTRAINT CK_Monitors_HttpMethod CHECK (HttpMethod IN (
                                                                     'GET',
