82421d11e387e4f8ade402f6f569bf1c3c86032c
pichus <dimatyzuk@gmail.com>
2026-06-30 16:00:45 +0300

    fix: add PollingTimeoutSeconds column to Monitors table

--- Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
+++ Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
@@ -9,9 +9,11 @@ IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
             LastCheckedAt          DATETIMEOFFSET,
             StatusId               UNIQUEIDENTIFIER NOT NULL,
             PollingIntervalSeconds INT              NOT NULL,
+            PollingTimeoutSeconds  INT              NOT NULL,
 
             CONSTRAINT PK_Monitors_Id PRIMARY KEY (Id),
             CONSTRAINT FK_Monitors_Status_Id FOREIGN KEY (StatusId) REFERENCES dbo.MonitorStatuses (Id),
-            CONSTRAINT CK_Monitors_Interval CHECK (PollingIntervalSeconds >= 60 AND PollingIntervalSeconds <= 24 * 60 * 60)
+            CONSTRAINT CK_Monitors_PollingIntervalSeconds CHECK (PollingIntervalSeconds >= 60 AND PollingIntervalSeconds <= 24 * 60 * 60),
+            CONSTRAINT CK_Monitors_PollingTimeoutSeconds CHECK (PollingIntervalSeconds >= 5 AND PollingIntervalSeconds <= 30)
         );
     END;
