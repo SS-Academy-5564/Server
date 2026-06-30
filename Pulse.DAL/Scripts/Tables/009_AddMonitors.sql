0f55671f5978502b1bd98114611084a5a835a54f
pichus <dimatyzuk@gmail.com>
2026-06-30 16:09:19 +0300

    fix: add HttpMethod column to Monitors table, add a check constraint for it

--- Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
+++ Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
@@ -5,6 +5,7 @@ IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
             Id                     UNIQUEIDENTIFIER NOT NULL DEFAULT NEWSEQUENTIALID(),
             Name                   VARCHAR(64)      NOT NULL,
             Url                    NVARCHAR(2083)   NOT NULL,
+            HttpMethod             NVARCHAR(20)     NOT NULL,
             CurrentValue           NVARCHAR(MAX),
             LastCheckedAt          DATETIMEOFFSET,
             StatusId               UNIQUEIDENTIFIER NOT NULL,
@@ -14,6 +15,14 @@ IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
             CONSTRAINT PK_Monitors_Id PRIMARY KEY (Id),
             CONSTRAINT FK_Monitors_Status_Id FOREIGN KEY (StatusId) REFERENCES dbo.MonitorStatuses (Id),
             CONSTRAINT CK_Monitors_PollingIntervalSeconds CHECK (PollingIntervalSeconds BETWEEN 60 AND 24 * 60 * 60),
-            CONSTRAINT CK_Monitors_PollingTimeoutSeconds CHECK (PollingIntervalSeconds BETWEEN 5 AND 30)
+            CONSTRAINT CK_Monitors_PollingTimeoutSeconds CHECK (PollingIntervalSeconds BETWEEN 5 AND 30),
+            CONSTRAINT CK_Monitors_HttpMethod CHECK (HttpMethod IN (
+                                                                    'GET',
+                                                                    'POST',
+                                                                    'PUT',
+                                                                    'PATCH',
+                                                                    'DELETE',
+                                                                    'HEAD',
+                                                                    'OPTIONS'))
         );
     END;
