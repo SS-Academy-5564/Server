405d2f6447cbe86ca507613eb965793bd9ca2009
pichus <dimatyzuk@gmail.com>
2026-06-30 15:51:22 +0300

    fix: rename Monitors table's PollingInterval column to PollingIntervalSeconds for better readability

--- Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
+++ Pulse.DAL/Scripts/Tables/009_AddMonitors.sql
@@ -8,10 +8,10 @@ IF OBJECT_ID(N'dbo.Monitors', N'U') IS NULL
             CurrentValue    NVARCHAR(MAX),
             LastCheckedAt   DATETIMEOFFSET,
             StatusId        UNIQUEIDENTIFIER NOT NULL,
-            PollingInterval INT              NOT NULL,
+            PollingIntervalSeconds INT              NOT NULL,
 
             CONSTRAINT PK_Monitors_Id PRIMARY KEY (Id),
             CONSTRAINT FK_Monitors_Status_Id FOREIGN KEY (StatusId) REFERENCES dbo.MonitorStatuses (Id),
-            CONSTRAINT CK_Monitors_Interval CHECK (PollingInterval > 0)
+            CONSTRAINT CK_Monitors_Interval CHECK (PollingIntervalSeconds > 0)
         );
     END;
