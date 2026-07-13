4bdadd089f9b5ba7ec786b74e6ad6614ce8a13fe
dianahavrysh <dhavrysh1205@gmail.com>
2026-07-14 01:04:26 +0300

    docs: update XML comment for OrganizationMembersResult

--- Pulse.BL/Features/Users/Members/OrganizationMembersResult.cs
+++ Pulse.BL/Features/Users/Members/OrganizationMembersResult.cs
@@ -1,6 +1,6 @@
 namespace Pulse.BL.Features.Users.Members;
 
 /// <summary>
-/// Represents a paged list of organization members.
+/// Represents the result of retrieving organization members.
 /// </summary>
 public sealed record OrganizationMembersResult(int TotalCount, IReadOnlyList<OrganizationMemberResult> Members);
